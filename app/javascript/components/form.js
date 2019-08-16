1ccc002112771236f406bd21ce5510711e6cd7ac
marian caron <caron.marian@gmail.com>
2019-08-16 16:54:48 +0200

    image hosting added

--- app/javascript/components/form.js
+++ app/javascript/components/form.js
@@ -1,1 +1,7 @@
-import select2 from 'select2'
+import select2 from 'select2';
+
+const selectMultiple = () => {
+  $('.select-multiple').select2();
+};
+
+export { selectMultiple };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,4 @@ import 'select2/dist/css/select2.css';
 import { selectMultiple } from '../components/form';
 
 console.log('Hello World from Webpacker')
+selectMultiple();
